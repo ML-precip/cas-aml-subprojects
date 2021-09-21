0108ba54f7d04293a13c7f6bef34bbe39234dec0
Pascal Horton <pascal.horton@giub.unibe.ch>
2021-09-21 11:32:33 +0200

    Code formatting.

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ CH_BOUNDING_BOX = [45.66, 47.87, 5.84, 10.98]
 
 # Data extraction functions for ERA5
 
+
 def rename_dimensions_variables(ds):
     """Rename dimensions and attributes of the given dataset to homogenize data."""
     if 'latitude' in ds.dims:
@@ -27,7 +28,7 @@ def get_era5_data(files, start, end):
 
     return ds
 
-    
+
 def extract_nearest_point_data(ds, lat, lon):
     """Return the time series data for the nearest grid point.
 
@@ -45,7 +46,7 @@ def extract_nearest_point_data(ds, lat, lon):
 
 def extract_points_around(ds, lat, lon, step_lat, step_lon, nb_lat, nb_lon):
     """Return the time series data for a grid point mesh around the provided coordinates.
-    
+
     Arguments:
     ds -- the dataset (xarray Dataset) to extract the data from
     lat -- the latitude coordinate of the center of the mesh
@@ -59,22 +60,25 @@ def extract_points_around(ds, lat, lon, step_lat, step_lon, nb_lat, nb_lon):
     z = xr.open_mfdataset(DATADIR + '/ERA5/geopotential/*.nc', combine='by_coords')
     a = extract_points_around(z, CH_CENTER[0], CH_CENTER[1], step_lat=1, step_lon=1, nb_lat=3, nb_lon=3)
     """
-    lats = np.arange(lat - step_lat * (nb_lat - 1) / 2, lat + step_lat * nb_lat / 2, step_lat)
-    lons = np.arange(lon - step_lon * (nb_lon - 1) / 2, lon + step_lon * nb_lon / 2, step_lon)
+    lats = np.arange(lat - step_lat * (nb_lat - 1) / 2,
+                     lat + step_lat * nb_lat / 2, step_lat)
+    lons = np.arange(lon - step_lon * (nb_lon - 1) / 2,
+                     lon + step_lon * nb_lon / 2, step_lon)
     xx, yy = np.meshgrid(lats, lons)
     xx = xx.flatten()
     yy = yy.flatten()
     xys = np.column_stack((xx, yy))
-    
+
     data = []
     for xy in xys:
         data.append(extract_nearest_point_data(ds, xy[0], xy[1]))
-    
+
     return data
 
-def get_data_mean_over_box(ds, lats, lons, level = 0):
+
+def get_data_mean_over_box(ds, lats, lons, level=0):
     """Extract data from points within a bounding box and process the mean.
-    
+
     Arguments:
     ds -- the dataset (xarray Dataset) to extract the data from
     lats -- the min/max latitude coordinates of the bounding box
@@ -105,11 +109,10 @@ def get_data_mean_over_box(ds, lats, lons, level = 0):
     return ds_box.mean(['lat', 'lon'])
 
 
-def get_data_mean_over_CH_box(ds, level = 0):
+def get_data_mean_over_CH_box(ds, level=0):
     """Extract data over the bounding box of Switzerland and return the mean time series.
-    
+
     Arguments:
     level -- the desired vertical level
     """
     return get_data_mean_over_box(ds, [CH_BOUNDING_BOX[0], CH_BOUNDING_BOX[1]], [CH_BOUNDING_BOX[2], CH_BOUNDING_BOX[3]], level)
-
