4f1a0cd5af6151c5f94a27a9e25b0b2030dfef35
Noelia Otero Felipe <noelia.oterofelipe@iass-potsdam.de>
2021-10-19 12:55:13 +0200

    Updating classical methods

--- utils_ml.py
+++ utils_ml.py
@@ -3,6 +3,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import precision_score, recall_score, roc_auc_score, roc_curve
 
 def split_data(df, yy_train, yy_test, attributes, ylabel):
     """"Split the data into train and test
@@ -48,6 +49,47 @@ def prepareData(dd, cat_var):
     df_prepared = full_pipeline.fit_transform(dd)
 
     return(full_pipeline)
+
+
+
+
+
+def evaluate_model(test_labels, train_labels, predictions, probs, train_predictions, train_probs):
+    """Compare machine learning model to baseline performance.
+    Computes statistics and shows ROC curve."""
+    
+    baseline = {}
+    
+    baseline['recall'] = recall_score(test_labels, [1 for _ in range(len(test_labels))])
+    baseline['precision'] = precision_score(test_labels, [1 for _ in range(len(test_labels))])
+    baseline['roc'] = 0.5
+    
+    results = {}
+    
+    results['recall'] = recall_score(test_labels, predictions)
+    results['precision'] = precision_score(test_labels, predictions)
+    results['roc'] = roc_auc_score(test_labels, probs)
+    
+    train_results = {}
+    train_results['recall'] = recall_score(train_labels, train_predictions)
+    train_results['precision'] = precision_score(train_labels, train_predictions)
+    train_results['roc'] = roc_auc_score(train_labels, train_probs)
+    
+    for metric in ['recall', 'precision', 'roc']:
+        print(f'{metric.capitalize()} Baseline: {round(baseline[metric], 2)} Test: {round(results[metric], 2)} Train: {round(train_results[metric], 2)}')
+    
+    # Calculate false positive rates and true positive rates
+    base_fpr, base_tpr, _ = roc_curve(test_labels, [1 for _ in range(len(test_labels))])
+    model_fpr, model_tpr, _ = roc_curve(test_labels, probs)
+
+    plt.figure(figsize = (8, 6))
+    plt.rcParams['font.size'] = 16
+    
+    # Plot both curves
+    plt.plot(base_fpr, base_tpr, 'b', label = 'baseline')
+    plt.plot(model_fpr, model_tpr, 'r', label = 'model')
+    plt.legend();
+    plt.xlabel('False Positive Rate'); plt.ylabel('True Positive Rate'); plt.title('ROC Curves');
 
 
 
